19bb52b54123cc6d28bd931bc61d6c95eed4b7c8
Helen Jones <helenfrancesjones@gmail.com>
2023-03-02 18:58:06 -0600

    extension for browser problems

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// TODO: add code here
+window.addEventListener('load', function (){
+
+    const container = document.getElementById('container');
+
+});
+
+
+fetch ('https://handlers.education.launchcode.org/static/astronauts.json').then
+    (function (response) {
+        response.json().then(function (data) {
+            console.log(data)
+
+ 
+    
+    for (let i=0; i<1; i++) {
+        let astronaut=data[i];
+        container.innerHTML +=
+        <div class='astronaut'>
+        <div class='bio'>
+            <h3>${astronaut.firstName}, ${astronaut.lastName}</h3>
+        <ul>
+            <li>Hours in space: ${astronaut.hoursInSpace}</li>
+            <li>Active: ${astronaut.active}</li>
+            <li>Skills: ${astronaut.skills}</li>
+        </ul>
+        </div>
+        <img class="avatar" src="images/mae-jemison.jpg"></img>
+        </div>
