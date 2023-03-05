a63025ddd54a5ece5eb06a7d3a3a721d38ce099d
Helen Jones <helenfrancesjones@gmail.com>
2023-03-04 19:49:00 -0600

    finishing studio pt 1

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 window.addEventListener('load', function (){
 
-    const container = document.getElementById('container');
-
-});
+    let container = document.getElementById('container');
 
 
 fetch ('https://handlers.education.launchcode.org/static/astronauts.json').then
     (function (response) {
-        response.json().then(function (data) {
-            console.log(data)
+        response.json().then(function (json) {
+            console.log(json)
 
- 
-    
+        
+        .then(function(json)) {
+        let astronautProfiles ="";
     for (let i=0; i<1; i++) {
-        let astronaut=data[i];
-        container.innerHTML +=
-        <div class='astronaut'>
-        <div class='bio'>
+        let astronaut=json[i];
+        astronautProfiles += `
+        <div class="astronaut">
+        <div class="bio">
             <h3>${astronaut.firstName}, ${astronaut.lastName}</h3>
         <ul>
             <li>Hours in space: ${astronaut.hoursInSpace}</li>
             <li>Active: ${astronaut.active}</li>
-            <li>Skills: ${astronaut.skills}</li>
+            <li>Skills: ${astronaut.skills.join(",")}</li>
         </ul>
         </div>
-        <img class="avatar" src="images/mae-jemison.jpg"></img>
+        <img class="avatar" src=${astronaut.picture}/>
         </div>
+        `
+        container.innerHTML = astronautProfiles;
+    }
+
+ 
+});
+});
+});
